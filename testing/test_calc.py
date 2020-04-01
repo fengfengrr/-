73376824a476b63f9e79355b687f856fa388c47a
fengfengrr <15645631628@163.com>
2020-04-01 20:22:00 +0800

    计算器测试用例

---
+++ testing/test_calc.py
@@ -0,0 +1,42 @@
+from PYTHON.calc import Calc
+
+
+class Testcalc:
+    def setup(self):
+        self.calc = Calc()
+
+    def test_add(self):
+        assert self.calc.add(1, 2) == 3
+
+    def test_add1(self):
+        assert self.calc.add(-1, 3) == 2
+
+    def test_add2(self):
+        assert self.calc.add(-1, -2) == -3
+
+    def test_add3(self):
+        assert self.calc.add(0, 0) == 0
+
+    def test_div1(self):
+        assert self.calc.div(0, 1) == 0
+
+    def test_div2(self):
+        assert self.calc.div(3, 1.5) == 2
+
+    def test_div(self):
+        assert self.calc.div(1, 2) == 0.5
+
+    def test_mul(self):
+        assert self.calc.mul(1, 2) == 2
+
+    def test_sub(self):
+        assert self.calc.sub(0, 0) == 0
+
+    def test_sub(self):
+        assert self.calc.sub(-1, -2) == -3
+
+    def test_sub(self):
+        assert self.calc.sub(3, 2) == 1
+
+    def test_sub(self):
+        assert self.calc.sub(1, -2) == 3
